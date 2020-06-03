2829b70c46134856b3f358b8d96a6c42b466b217
Zeca Feitosa <zeca.feitosa@vtex.com.br>
2020-06-03 20:16:05 -0300

    Form validation with Formik and Yup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const FormSchema = Yup.object().shape({
+		name: Yup.string()
+			.min(2, 'Too Short!')
+			.max(30, 'Too Long!')
+			.required('Required'),
+		email: Yup.string()
+			.email('Invalid email')
+			.required('Required')
+	});
+
+	const { handleSubmit, handleChange, values, errors, touched } = useFormik({
+		initialValues: {
+			name: '',
+			email: ''
+		},
+		validationSchema: FormSchema,
+		onSubmit: values => {
+			alert(JSON.stringify(values, null, 2));
+		}
+	});
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo"/>
+			</header>
+
+			<p>
+				Edit <code>src/App.js</code> and save to reload.
+			</p>
+
+			<form onSubmit={handleSubmit}>
+				<label htmlFor="name">Name</label>
+				<input
+					id="name"
+					type="text"
+					name="name"
+					onChange={handleChange}
+					values={values.name}
+				/>
+				{errors.name && touched.name && (
+					<span className="inputError">{errors.name}</span>
+				)}
+
+				<label htmlFor="email">Email</label>
+				<input
+					id="email"
+					type="email"
+					name="email"
+					onChange={handleChange}
+					values={values.email}
+				/>
+				{errors.email && touched.email && (
+					<span className="inputError">{errors.email}</span>
+				)}
+				<button	type="submit">Submit</button>
+			</form>
+		</div>
+	);
 }
 
 export default App;
